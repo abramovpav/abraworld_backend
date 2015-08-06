6417c9a3e15f63fd7bc39899eac7d5b0859b645d
Pavel Abramov <abramovpav@gmail.com>
2015-08-06 20:42:54 +0300

    Added profiles application. Added user serialization

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,7 @@
-from django.http import Http404
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from blog.models import Article
-from blog.serializers import ArticleSerializer
 from rest_framework import generics
 
+from blog.models import Article
+from blog.serializers import ArticleSerializer
 
 
 class ArticlesList(generics.ListCreateAPIView):
