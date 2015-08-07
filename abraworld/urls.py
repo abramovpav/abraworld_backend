3f9c92e63f1a38d113384629ba5dc8ec8f66242b
Pavel Abramov <abramovpav@gmail.com>
2015-08-07 19:40:05 +0300

    Changed urls

--- abraworld/urls.py
+++ abraworld/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     # url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('blog.urls')),
+    url(r'^api/', include('blog.urls')),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
